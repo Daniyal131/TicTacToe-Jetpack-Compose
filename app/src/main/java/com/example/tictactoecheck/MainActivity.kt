8656995f05241f3ec6710a1edd9819b4d7e95f6f
Daniyal <khan.muhammaddaniyal124@gmail.com>
2023-08-20 23:49:02 +0500

    First Comit

---
+++ app/src/main/java/com/example/tictactoecheck/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.tictactoecheck
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.tictactoecheck.ui.theme.TicTacToeCheckTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TicTacToeCheckTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GameScreen(
+                        // turn hint should be removed by win notification
+                        //state is getting zero on orientation configuration change
+                        // is not working on landscape orientation
+
+                    )
+                }
+            }
+        }
+    }
+}
